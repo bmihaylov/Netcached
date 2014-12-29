89f0e9e36b8f339b36cfeec9545f0025a251ff5c
Bozhidar Mihaylov <bmichailov@gmail.com>
2014-12-30 00:11:29 +0200

    added service contract

---
+++ Netcached/INetcached-Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Netcached
+{
+    [ServiceContract]
+    public interface INetcached_Server
+    {
+        [OperationContract]
+        bool Set(string key, byte[] data, bool replace = true);
+
+        [OperationContract]
+        bool Get(string key);
+    }
+}
---
+++ Netcached/Netcached-Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Netcached
+{
+    public class Netcached_Server : INetcached_Server
+    {
+        public bool Set(string key, byte[] data, bool replace = true)
+        {
+            return false;
+        }
+
+        public bool Get(string key)
+        {
+            return false;
+        }
+    }
+}
