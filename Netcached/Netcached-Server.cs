b1ef4470e38f8ac8b5271241ce3780270cdd645a
Bozhidar Mihaylov <bmichailov@gmail.com>
2014-12-30 10:28:09 +0200

    Initial decision for Get design

--- Netcached/Netcached-Server.cs
+++ Netcached/Netcached-Server.cs
@@ -9,19 +9,39 @@ namespace Netcached
 {
     public class Netcached_Server : INetcached_Server
     {
+        /// <summary>
+        /// Set the value for the key with data, replacing it if the key is already present.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="data"></param>
+        /// <param name="priority">All keys with priority X are ordered before all keys with priority X+1.
+        /// That is: if a key1 has priority 0 and key2 has priority 1 -> key1 will be evicted first, should
+        /// the need arise (due to lack of memory).
+        /// Keys with the same priority are ordered according to the time stamp of their last use.</param>
+        /// <returns></returns>
         public bool Set(string key, byte[] data, int priority = 0)
         {
             return false;
         }
 
+        /// <summary>
+        /// Delete the entry the specified key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public bool Delete(string key)
         {
             return false;
         }
 
-        public bool Get(string key)
+        /// <summary>
+        /// Returns the value for the specified key if present, and null if the operation failed.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public byte[] Get(string key)
         {
-            return false;
+            return null;
         }
     }
 }
