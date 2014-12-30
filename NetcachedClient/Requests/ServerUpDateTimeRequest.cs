6f42b311e86022617790d58c8a71e42803dcebd3
Veselin-Genadiev <genadiev.veselin@gmail.com>
2014-12-30 23:17:56 +0200

    [Feature] Add sample Netcached client.

---
+++ NetcachedClient/Requests/ServerUpDateTimeRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace NetcachedClient.Requests
+{
+    [DataContract]
+    public class ServerUpDateTimeRequest : NetcachedRequest
+    {
+        /// <summary>
+        /// The format in which the datetime will be returned.
+        /// </summary>
+        [DataMember]
+        public string Format { get; set; }
+
+        /// <summary>
+        /// The pattern in which the key will be set-up. Override this method for every
+        /// type of NetcachedRequest you are constructing. Keep it unique for different
+        /// type of requests.
+        /// </summary>
+        /// <returns>A unique key for the request.</returns>
+        public override string GetKey()
+        {
+            return "ServerUpDateTime";
+        }
+    }
+}
---
+++ NetcachedClient/Responses/ServerUpDateTimeResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace NetcachedClient.Responses
+{
+    [DataContract]
+    public class ServerUpDateTimeResponse
+    {
+        /// <summary>
+        /// The last DateTime when the server was driven up.
+        /// </summary>
+        [DataMember]
+        public string DateTime { get; set; }
+    }
+}
