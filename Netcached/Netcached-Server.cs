eca38f5cf77d13d8f3ce2abdb49c1766a2da035a
Bozhidar Mihaylov <bmichailov@gmail.com>
2014-12-31 01:12:21 +0200

    Added implementation of Set

--- Netcached/Netcached-Server.cs
+++ Netcached/Netcached-Server.cs
@@ -11,33 +11,46 @@ namespace Netcached
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class Netcached_Server : INetcached_Server
     {
-        int a = 0;
-        struct OrderEntry : IComparable<OrderEntry>
+        struct Entry : IComparable<Entry>
         {
-            public OrderEntry(string key, DateTime lastAccess, int priority)
+            //TODO change order of arguments
+            public Entry(string key, long lastAccess, int priority, byte[] data)
             {
                 this.key = key;
                 this.lastAccess = lastAccess;
                 this.priority = priority;
+                this.data = data;
+                // 16 is the overehead of an empty string
+                // first constant is due to the overhead of the data structures and the stored PriorityQueueHandles
+                this.size = 160 +  (16 + key.Length * sizeof(char) + sizeof(long) + sizeof(int) + data.Length + sizeof(long));
             }
 
-            int IComparable<OrderEntry>.CompareTo(OrderEntry other)
+            int IComparable<Entry>.CompareTo(Entry other)
             {
                 if (priority == other.priority)
                 {
-                    TimeSpan diff = lastAccess - other.lastAccess;
-                    return TimeSpan.Compare(diff, TimeSpan.Zero);
+                    if (lastAccess == other.lastAccess)
+                    {
+                        return 0;
+                    }
+                    return lastAccess - other.lastAccess > 0 ? 1 : -1;
                 }
                 return priority - other.priority;
             }
 
             public string key;
-            public DateTime lastAccess;
-            public int priority;
+            long lastAccess; // this is DateTime.Now.Ticks for the last access to the key
+            int priority;
+            public byte[] data;
+            // this is only an approximation of the memory in bytes taken due to this particular Entry
+            public long size; 
         }
 
-        private IntervalHeap<OrderEntry> order = new IntervalHeap<OrderEntry>();
-        private Dictionary<string, byte[]> keyValueStore = new Dictionary<string,byte[]>();
+        private IntervalHeap<Entry> priorityQueue = new IntervalHeap<Entry>();
+        private Dictionary<string, IPriorityQueueHandle<Entry>> keyHandleStore =
+            new Dictionary<string, IPriorityQueueHandle<Entry>>();
+        long usedSpace = 0;
+        long allowedSpace = 128 * 1000 * 1000;
 
         /// <summary>
         /// Set the value for the key with data, replacing it if the key is already present.
@@ -48,9 +61,50 @@ namespace Netcached
         /// That is: if a key1 has priority 0 and key2 has priority 1 -> key1 will be evicted first, should
         /// the need arise (due to lack of memory).
         /// Keys with the same priority are ordered according to the time stamp of their last use.</param>
-        /// <returns>Wheter the operation was successful</returns>
+        /// <returns>Whether the operation was successful</returns>
         public bool Set(string key, byte[] data, int priority = 0)
         {
+            Entry newEntry = new Entry(key, DateTime.Now.Ticks, priority, data);
+            IPriorityQueueHandle<Entry> oldHandle = null;
+            bool enoughSpace = true;
+
+            if (keyHandleStore.TryGetValue(key, out oldHandle))
+            {
+                long sizeDifference = newEntry.size - priorityQueue[oldHandle].size;
+                if (usedSpace - sizeDifference > allowedSpace)
+                {
+                    enoughSpace = freeSpace(sizeDifference);
+                }
+            }
+            else if (usedSpace + newEntry.size > allowedSpace)
+            {
+                enoughSpace = freeSpace(newEntry.size);
+            }
+
+
+            if (enoughSpace)
+            {
+                priorityQueue.Add(ref oldHandle, new Entry(key, DateTime.Now.Ticks, priority, data));
+                keyHandleStore.Add(key, oldHandle);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool freeSpace(long desiredSpace)
+        {
+            if (desiredSpace > allowedSpace)
+            {
+                return false;
+            }
+
+            while (allowedSpace - usedSpace < desiredSpace)
+            {
+                Entry entry = priorityQueue.DeleteMin();
+                usedSpace -= entry.size;
+                keyHandleStore.Remove(entry.key);
+            }
             return false;
         }
 
@@ -71,7 +125,7 @@ namespace Netcached
         /// <returns></returns>
         public byte[] Get(string key)
         {
-            return new byte[]{(byte)++a};
+            return null;
         }
     }
 }
