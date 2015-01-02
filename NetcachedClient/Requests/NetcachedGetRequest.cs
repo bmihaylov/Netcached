21ab3ebac372145b7b9e3dc03b7e4a54d435f075
Veselin-Genadiev <genadiev.veselin@gmail.com>
2015-01-03 00:02:09 +0200

    Add missing files

---
+++ NetcachedClient/Requests/NetcachedGetRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace NetcachedClient.Requests
+{
+    /// <summary>
+    /// GetRequest that is meant for memcaching.
+    /// </summary>
+    [DataContract]
+    public class NetcachedGetRequest : NetcachedRequest
+    {
+    }
+}
---
+++ NetcachedClient/Serializing/DataSerializer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web;
+
+namespace NetcachedClient.Serializing
+{
+    public class DataSerializer
+    {
+        public static byte[] Serialize<T>(T data)
+        {
+            byte[] serialized = null;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                try
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(ms, data);
+                    serialized = ms.ToArray();
+                }
+                catch (SerializationException e)
+                {
+                    Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                    throw;
+                }
+            }
+
+            return serialized;
+        }
+
+        public static T Deserialize<T>(byte[] data)
+        {
+            T deserialized = default(T);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                try
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    object deserializedObject = formatter.Deserialize(ms);
+
+                    if (deserializedObject != null && deserializedObject is T)
+                    {
+                        deserialized = (T)deserializedObject;
+                    }
+                }
+                catch (SerializationException e)
+                {
+                    Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                    throw;
+                }
+            }
+
+            return deserialized;
+        }
+    }
+}
