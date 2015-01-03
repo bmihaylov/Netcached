0251270f321b063cf2d14fd9d81f5f114796e597
Veselin-Genadiev <genadiev.veselin@gmail.com>
2015-01-03 20:25:51 +0200

    Add structure for Fibonacci page.

---
+++ Netcached.Example/Controllers/FibonacciController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Netcached.Example.Controllers
+{
+    public class FibonacciController : Controller
+    {
+        // 
+        // GET: /Fibonacci/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Fibonacci/index
+
+    }
+}
