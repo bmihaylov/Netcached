b6ab9708ffbbb1eab5e9704e7bc49095fd422d08
Bozhidar Mihaylov <bmichailov@gmail.com>
2015-01-03 21:48:10 +0200

    Added Model and Controller for example project

--- Netcached.Example/Controllers/FibonacciController.cs
+++ Netcached.Example/Controllers/FibonacciController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Netcached.Client;
+using Netcached.Example.Models;
 
 namespace Netcached.Example.Controllers
 {
+    [RequireHttps]
     public class FibonacciController : Controller
     {
         // 
@@ -15,7 +18,32 @@ namespace Netcached.Example.Controllers
             return View();
         }
 
-        // GET: /Fibonacci/index
+        // GET: /Fibonacci/position
+        public ActionResult Index(int position)
+        {
 
+            NetcachedClient client = new NetcachedClient();
+            int? value = client.Get<int?>(position.ToString());
+            if (value != null)
+            {
+                return View(new FibonacciViewModel(){Position = position, Value = (int)value});
+            }
+
+            return View(new FibonacciViewModel(){Position = position, Value = Calculate(position)});
+        }
+
+        /// <summary>
+        /// An intentionaly slow calculation of Fibonacci numbers so as to illustrate the caching
+        /// </summary>
+        /// <param name="position">The position of the number in the Fibonacci sequence, starting from 0</param>
+        /// <returns></returns>
+        private static Int64 Calculate(Int32 position)
+        {
+            if (position == 0 || position == 1)
+            {
+                return position;
+            }
+            return Calculate(position - 2) + Calculate(position - 1);
+        }
     }
 }
--- Netcached.Example/Models/FibonacciViewModel.cs
+++ Netcached.Example/Models/FibonacciViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace Netcached.Example.Models
 {
     public class FibonacciViewModel
     {
+        [Display(Name = "Position")]
+        [DataType(DataType.Text)]
+        public Int32 Position { get; set; }
+
+        [Display(Name = "Value")]
+        [DataType(DataType.Text)]
+        public Int64 Value { get; set; }
     }
 }
